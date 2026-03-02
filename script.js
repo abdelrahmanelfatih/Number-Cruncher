7b3cf3d620c178c2c4c8febec1d3fe1c447a50a1
Abdelrahman <nar7gamer@gmail.com>
2026-03-02 16:18:08 +0300

    Added some styling

--- script.js
+++ script.js
@@ -10,22 +10,26 @@ const clearBtn = document.getElementById("clearBtn");
 button.addEventListener("click", function() {
     if (input.value === ""){
         result.textContent = "Please enter a number first.";
+        result.style.color = "red";
         return;
     }
-    
+
     let value = Number(input.value);
 
     if(!Number.isInteger(value)){
         result.textContent = "Please enter an Integer";
+        result.style.color = "red";
         return;
     }
     numbers.push(value);
 
     if(value % 2 === 0){
         result.textContent = "The number " + value + " is Even";
+        result.style.color = "blue";
     }
     else{
         result.textContent = "The number " + value + " is Odd";
+        result.style.color = "red";
     }
 
     numbersList.textContent = "Numbers: " + numbers.join(", ");

f83b80ab951b70e8ce1e662d8f36f6b9847f787b
Abdelrahman <nar7gamer@gmail.com>
2026-03-02 16:01:28 +0300

    Added a clear button

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ const button = document.getElementById("analyzeBtn");
 const result = document.getElementById("result");
 const numbersList = document.getElementById("numberList");
 const totalSum = document.getElementById("totalSum");
+const clearBtn = document.getElementById("clearBtn");
 
 button.addEventListener("click", function() {
+    if (input.value === ""){
+        result.textContent = "Please enter a number first.";
+        return;
+    }
     let value = Number(input.value);
     numbers.push(value);
 
@@ -22,11 +27,18 @@ button.addEventListener("click", function() {
 
     let sum = 0;
 
-    for (let i = 0; i < numbers.length; i++){
-        sum += numbers[i];
+    for (let number of numbers){
+        sum += number;
     }
 
     totalSum.textContent = "Total Sum: " + sum;
 
     input.value = "";
+});
+
+clearBtn.addEventListener("click", function(){
+    numbers = [];
+    numbersList.textContent = "";
+    totalSum.textContent = "";
+    result.textContent = "";
 });
